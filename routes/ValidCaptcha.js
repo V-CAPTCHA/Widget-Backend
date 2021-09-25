9345915c02ae76284d37bac242be7037b88ae809
THANATATH ASSAVARATTANAKORN <62015048@kmitl.ac.th>
2021-09-26 06:41:08 +0700

    fix compare bug & add parameter checking

--- routes/ValidCaptcha.js
+++ routes/ValidCaptcha.js
@@ -5,6 +5,15 @@ const models = require('../DB_Connection');
 
 //Router Path
 server.get('/', async (req, res) => {
+  // filter Null Parameter
+  if (
+    req.query.actionReply == null ||
+    req.query.actionID == null ||
+    req.query.key == null
+  ) {
+    res.send('Problem !! Wrong Or Null Parameter');
+    return 0;
+  }
 
   //setup variable
   reqAction = {
@@ -13,20 +22,26 @@ server.get('/', async (req, res) => {
     key: req.query.key,
   };
 
-  //quiery from Request
-  var queryAction = await ActionAuthen_Query(reqAction.actionID);
+  
+  try { //try main process
+    //quiery from Request
+    var queryAction = await ActionAuthen_Query(reqAction.actionID);
 
-  //compare Phase with Message
-  var compareACtion = compareObject(reqAction, queryAction) //temp variable
-  var valid = compareACtion[0]
-  var validMessage = compareACtion[1]
+    //compare Phase with Message
+    var compareACtion = compareObject(reqAction, queryAction); //temp variable
+    var valid = compareACtion[0];
+    var validMessage = compareACtion[1];
 
-  //return Statement
-  if (valid) {
-    ActionAuthen_Write(reqAction.actionID, reqAction.actionReply, 'PASSES');
-    res.json({valid:'Valid',Message:validMessage});
-  } else {
-    res.json({valid:'Not valid',Message:validMessage});
+    //return Statement
+    if (valid) {
+      ActionAuthen_Write(reqAction.actionID, reqAction.actionReply, 'PASSES');
+      res.json({ valid: 'Valid', Message: validMessage });
+    } else {
+      res.json({ valid: 'Not valid', Message: validMessage });
+      ActionAuthen_Write(reqAction.actionID, reqAction.actionReply, 'WRONG');
+    }
+  } catch (error) {
+    res.json({ valid: 'Not valid', Message: error });
   }
 });
 
@@ -44,7 +59,7 @@ async function ActionAuthen_Write(actionID, actionReply, valid) {
   var req = await models.authen_action.update(
     {
       action_reply: actionReply,
-      //action_end: Sequelize.literal('CURRENT_TIMESTAMP'),
+      action_end: Sequelize.literal('CURRENT_TIMESTAMP'),
       action_checked: true, //make TRUE Value static in Action authen Table
       action_valid: valid,
     },
@@ -62,15 +77,18 @@ function compareObject(req, db) {
     //check if it's compared
     return [false, 'This action is checked'];
   }
+
   var timeCompare = new Date(); // create time compare value for check outded authen
-  if (timeCompare.setSeconds(-60) >= db.action_end) {
+  var timeDB = new Date(db.action_create);
+  if (timeCompare.setSeconds(-60) > timeDB) {
     return [false, 'This action is out of 60 Second'];
   } //if out of 60 sec it's not valid
-  return [(
+  return [
     req.actionID == db.action_id &&
-    db.dataset.dataset_reply.split(',').indexOf(req.actionReply) > -1 &&
-    req.key == db.key_value
-  ),'PASS'];
+      db.dataset.dataset_reply.split(',').indexOf(req.actionReply) > -1 &&
+      req.key == db.key_value,
+    'Checked',
+  ];
 }
 
 module.exports = server;
