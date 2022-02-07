d618c9c3a59fa3f75bf141893a9b1f0bf2e26451
THANATATH <thanatuth.euro@gmail.com>
2022-02-07 17:07:52 +0700

    make rate-limit & helmet in project

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 var cors = require('cors')
 const server = express();
+const rateLimit = require('express-rate-limit') //use this to limit the number of requests per second
+const helmet = require("helmet"); //use this to secure from common attacks
 server.use(cors("*")) //fix cors policy
 server.disable("x-powered-by"); //Security Hotspots Fix
 server.set('trust proxy')
@@ -18,6 +20,17 @@ server.listen(process.env.Express_Port, () => {
   console.log('Start server at port', process.env.Express_Port);
 });
 
+//rate limit for captcha
+const limiter = rateLimit({
+	windowMs: 3 * 60 * 1000, // 3 minutes
+	max: 10, // Limit each IP to 10 requests (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+server.use(limiter) //use limiter
+server.use(helmet()); //use helmet
+
 server.get('/', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.json(String('Widget API'));
@@ -26,6 +39,8 @@ server.get('/', (req, res) => {
 //Express Router Use
 server.use('/GetCaptcha',GetCaptcha)
 server.use('/ValidCaptcha',ValidCaptcha)
+
+ 
 
 
 
